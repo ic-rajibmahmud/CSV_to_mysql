5421de4f374102c26e97d6d4b45b62a1a88c1784
Rajib Mahmud <62582727+ic-rajibmahmud@users.noreply.github.com>
2023-04-16 11:15:15 +0600

    Create execute.py

---
+++ execute.py
@@ -0,0 +1,32 @@
+import os
+import mysql.connector
+
+# Define the database connection parameters
+db_config = {
+    'user': 'username',
+    'password': 'password',
+    'host': 'hostname',
+    'database': 'dbname',
+}
+
+# Define the folder path where the CSV files are located
+folder_path = "N:/hsales/"
+
+# Create a MySQL connection
+cnx = mysql.connector.connect(**db_config)
+cursor = cnx.cursor()
+
+# Loop through each file in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith('.csv'):
+        # Get the full path to the CSV file
+        file_path = os.path.join(folder_path, filename)
+        # Define the MySQL LOAD DATA INFILE statement
+        load_data_stmt = f"LOAD DATA INFILE '{file_path}' INTO TABLE sales_data FIELDS TERMINATED BY ',' LINES TERMINATED BY '\\n' "
+        # Execute the LOAD DATA INFILE statement
+        cursor.execute(load_data_stmt)
+        cnx.commit()
+
+# Close the MySQL connection
+cursor.close()
+cnx.close()
